077cc226b50bddd97bcdf28bbb3faef8ffcb5c91
Sagar <sriramkanakam87@gmail.com>
2024-07-10 14:48:16 +0200

    chore: sample resource fine tuning (wip)

--- app/Filament/Company/Resources/SampleResource.php
+++ app/Filament/Company/Resources/SampleResource.php
@@ -5,6 +5,8 @@ namespace App\Filament\Company\Resources;
 use App\Filament\Company\Resources\SampleResource\Pages;
 use App\Filament\Company\Resources\SampleResource\RelationManagers;
 use App\Models\Sample;
+use App\Models\SpectrumType;
+use App\Models\Device;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,6 +15,9 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use RalphJSmit\Filament\MediaLibrary\Forms\Components\MediaPicker;
+use Filament\Forms\Get;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Section;
 
 class SampleResource extends Resource
 {
@@ -26,26 +31,76 @@ class SampleResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('device_id')
-                    ->relationship('device', 'name'),
-                // Forms\Components\Select::make('company_id')
-                //     ->relationship('company', 'name')
-                //     ->required(),
-                Forms\Components\TextInput::make('identifier')
-                    ->maxLength(255),
-                Forms\Components\Select::make('solvent_id')
-                    ->relationship('solvent', 'name'),
-                Forms\Components\Select::make('molecule_id')
-                    ->relationship('molecule', 'name'),
-                Forms\Components\TextInput::make('spectrum_type')
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('instructions')
-                    ->columnSpanFull(),
-                MediaPicker::make('featured_image_id')
-                    ->label('Choose images'),
-                Forms\Components\TextInput::make('priority')
-                    ->required()
-                    ->maxLength(255),
+                Section::make('Tracking info')
+                    ->schema([
+                        Forms\Components\TextInput::make('reference_id')
+                            ->label('Sample ID')
+                            ->prefix('NMR-' . date("Ym") . '-ID-')
+                            ->placeholder('Enter a keyword for personal use'),
+                    ])
+                    ->columns(2),
+                Section::make('Configuration')
+                    ->schema([
+
+                        Forms\Components\Select::make('device_id')
+                            ->relationship('device', 'name')
+                            ->live(),
+                        // Forms\Components\Select::make('company_id')
+                        //     ->relationship('company', 'name')
+                        //     ->label('Group Name')
+                        //     ->required(),
+                        // Forms\Components\TextInput::make('identifier')
+                        //     ->maxLength(255),
+
+                        Forms\Components\TextInput::make('priority')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\Select::make('spectrum_type')
+                            ->multiple()
+                            ->options(function (Get $get) {
+                                return Device::find($get('device_id'))?->spectrumTypes()->pluck('name', 'id');
+                            }),
+                        Forms\Components\TextInput::make('other_nuclei')
+                            ->label('Other Nuclei (please specify)')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\Checkbox::make('is_automation')
+                            ->label('Automation')
+                            ->helperText('Do you want to process this sample automatically?'),
+                    ])
+                    ->columns(2),
+                Section::make('Structure info')
+                    ->schema([
+                        Fieldset::make('Attach a mol file')
+                        ->schema([
+                            MediaPicker::make('featured_molecule_id')
+                                ->label(''),
+                        ])
+                    ])
+                    ->columns(2),
+                Section::make('Sample info')
+                    ->schema([
+                        Forms\Components\Select::make('solvent_id')
+                            ->relationship('solvent', 'name'),
+                        Forms\Components\Select::make('molecule_id')
+                            ->relationship('molecule', 'name'),
+                        Forms\Components\Textarea::make('instructions')
+                            ->label('Special care for sample')
+                            ->columnSpanFull(),
+                        Fieldset::make('Attach a file for the operator')
+                            ->schema([
+                                MediaPicker::make('featured_image_id')
+                                    ->label(''),
+                            ])
+                    ])
+                    ->columns(2),
+
+
+
+
+
+
+
                 // Forms\Components\Select::make('operator_id')
                 //     ->relationship('user', 'name'),
             ]);
