90056caba4e3dd17726345d3f8cbff3e22b06375
Sagar <sriramkanakam87@gmail.com>
2024-07-15 11:58:20 +0200

    feat: ticker is added

---
+++ database/seeders/TickerSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TickerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('tickers')->insert([
+            'type' => 'App\Models\Ticker',
+            'index' => '1000',
+            'meta' => null,
+        ]);
+    }
+}
