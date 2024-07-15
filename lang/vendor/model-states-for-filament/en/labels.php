0141583405e28e4e739a680ef4b86c2753c3f587
Sagar <sriramkanakam87@gmail.com>
2024-07-15 12:09:02 +0200

    feat: installed model states package

---
+++ lang/vendor/model-states-for-filament/en/labels.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'all' => 'All',
+    'hide_state' => 'Hide :state',
+    'transition_to_state' => 'Transition to :state',
+    'transitioned' => 'Transitioned',
+];
